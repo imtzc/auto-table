9693ba4b466cb9ebfc9b6cbc8808183c1ab4f97c
tangzc <imtzc@qq.com>
2024-02-02 13:38:13 +0800

    代码优化

--- auto-table-test/auto-table-spring-boot-dynamicdatasource-test/src/main/java/com/tangzc/autotable/test/springboot/dynamicdatasource/autotableconfig/DynamicDataSourceHandler.java
+++ auto-table-test/auto-table-spring-boot-dynamicdatasource-test/src/main/java/com/tangzc/autotable/test/springboot/dynamicdatasource/autotableconfig/DynamicDataSourceHandler.java
@@ -1,4 +1,4 @@
-package com.tangzc.autotable.test.springboot.dynamicdatasource;
+package com.tangzc.autotable.test.springboot.dynamicdatasource.autotableconfig;
 
 import com.tangzc.autotable.core.dynamicds.IDataSourceHandler;
 import com.tangzc.autotable.test.springboot.dynamicdatasource.dynamicdatasourceframe.Ds;
@@ -10,16 +10,19 @@ public class DynamicDataSourceHandler implements IDataSourceHandler<String> {
 
     @Override
     public void useDataSource(String dataSourceName) {
+        // 切换数据源
         DynamicDataSourceContextHolder.setContextKey(dataSourceName);
     }
 
     @Override
     public void clearDataSource(String dataSourceName) {
+        // 清除数据源
         DynamicDataSourceContextHolder.removeContextKey();
     }
 
     @Override
     public String getDataSourceName(Class<?> clazz) {
+        // 根据实体类获取对应的数据源名称
         Ds ds = clazz.getAnnotation(Ds.class);
         if (ds != null) {
             return ds.value();
---
+++ auto-table-test/auto-table-spring-boot-dynamicdatasource-test/src/main/java/com/tangzc/autotable/test/springboot/dynamicdatasource/autotableconfig/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 这里是AutoTable的多数据源配置
+ */
+package com.tangzc.autotable.test.springboot.dynamicdatasource.autotableconfig;
