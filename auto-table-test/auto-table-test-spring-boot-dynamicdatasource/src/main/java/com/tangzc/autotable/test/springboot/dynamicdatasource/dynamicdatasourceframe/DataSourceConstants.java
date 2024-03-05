5c14c4403bd25ffd0594fcba2187a37b3021ecee
tangzc <imtzc@qq.com>
2024-03-05 09:40:11 +0800

    删除test模块，不发布

--- auto-table-test/auto-table-test-spring-boot-dynamicdatasource/src/main/java/com/tangzc/autotable/test/springboot/dynamicdatasource/dynamicdatasourceframe/DataSourceConstants.java
+++
@@ -1,6 +0,0 @@
-package com.tangzc.autotable.test.springboot.dynamicdatasource.dynamicdatasourceframe;
-
-public interface DataSourceConstants {
-    String DS_KEY_MASTER = "master";
-    String DS_KEY_SLAVE = "slave";
-}
