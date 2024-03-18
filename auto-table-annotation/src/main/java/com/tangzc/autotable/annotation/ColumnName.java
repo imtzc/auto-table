fbbf48e550a61055ec7ff2ca23aded1694e9ded1
lhzsdnu <2383376138@qq.com>
2024-03-18 12:21:36 +0000

    update auto-table-annotation/src/main/java/com/tangzc/autotable/annotation/ColumnName.java.
    Signed-off-by: lhzsdnu <2383376138@qq.com>

--- auto-table-annotation/src/main/java/com/tangzc/autotable/annotation/ColumnName.java
+++ auto-table-annotation/src/main/java/com/tangzc/autotable/annotation/ColumnName.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
  *
  * @author don
  */
-@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ColumnName {
