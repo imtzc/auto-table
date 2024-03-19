a07b421abf3c8512e24055bdb3a3e961dd12674f
tangzc <imtzc@qq.com>
2024-03-19 15:09:47 +0800

    :memo: 优化README及部分代码

--- auto-table-core/src/main/java/com/tangzc/autotable/core/constants/DatabaseDialect.java
+++ auto-table-core/src/main/java/com/tangzc/autotable/core/constants/DatabaseDialect.java
@@ -5,8 +5,8 @@ public interface DatabaseDialect {
     String MySQL = "MySQL";
     String PostgreSQL = "PostgreSQL";
     String SQLite = "SQLite";
+    String H2 = "H2";
     String Oracle = "Oracle";
     String SQLServer = "SQLServer";
     String DB2 = "DB2";
-    String H2 = "H2";
 }
